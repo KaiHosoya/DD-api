e73cd0313f26315e31673f76b6c6f82461a4190d
KaiHosoya <kaihosoya711@gmail.com>
2022-12-19 11:09:57 +0900

    追加

--- server.py
+++ server.py
@@ -4,22 +4,28 @@ from flask_cors import CORS
 # from DD import get_df
 import DD
 
-app = Flask(__name__, static_folder="./build/static", template_folder="./build")
-CORS(app) #Cross Origin Resource Sharing
+app = Flask(__name__, static_folder="./build/static",
+            template_folder="./build")
+CORS(app)  # Cross Origin Resource Sharing
+
 
 @app.route("/", methods=['GET'])
 def index():
     return "text parser:)"
 
-@app.route("/get_df", methods=['GET','POST'])
+
+@app.route("/get_df", methods=['GET', 'POST'])
 def parse():
     data = request.get_json()
     id = data['id']
 
     res = DD.DD.get_df(id, 10, 10)
-    response = {'result': res}
-    #print(response)
-    return make_response(jsonify(response))
+    print(res)
+    # response = {'result': res}
+    # print(response)
+    # return make_response(jsonify(res))
+    return res
+
 
 if __name__ == "__main__":
     app.debug = True
